cac1635cabd0c9b84f63eb9606619a9ac51890ea
jcc <370234915@qq.com>
2021-07-19 14:53:20 +0800

    update

--- Modules/LivewireCore/Support/helpers.php
+++ Modules/LivewireCore/Support/helpers.php
@@ -34,7 +34,7 @@ if (!function_exists('find_controller_by_url')) {
                 $path = '/'.ltrim($path, '/');
                 return preg_match($route->getCompiled()->getRegex(), rawurldecode($path));
             })($r,$path)){
-                dd($r);
+                dd($r->getController());
                 return $r->getController();
             }
 
