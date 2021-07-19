9b0cfbf895b3bcf0abaf71e91908f0dd315c91ec
jcc <370234915@qq.com>
2021-07-19 14:44:54 +0800

    update

--- Modules/Abc/Resources/views/hello/index.blade.php
+++
@@ -1,6 +0,0 @@
-<x-back-container>
-    @livewire('backend.widgets.lists',[
-        'widget' =>$widget,
-        'prefix' => 'list'
-    ])
-</x-back-container>
--- Modules/Abc/Resources/views/hello/update.blade.php
+++
@@ -1,3 +0,0 @@
-<x-back-container>
-    <x-back-form :widget="$widget"></x-back-form>
-</x-back-container>
