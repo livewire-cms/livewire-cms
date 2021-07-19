f79afe44c476df7426c5d0549f98f27d01a55ef1
jcc <370234915@qq.com>
2021-07-19 18:15:50 +0800

    update

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -9,7 +9,11 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
-][$maxWidth ?? '2xl'];
+    '3xl' => 'sm:max-w-3xl',
+    '4xl' => 'sm:max-w-4xl',
+    '5xl' => 'sm:max-w-5xl',
+    '6xl' => 'sm:max-w-6xl',
+][$maxWidth ?? '4xl'];
 @endphp
 
 <div
