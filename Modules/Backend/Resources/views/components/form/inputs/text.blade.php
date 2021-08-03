678670af0c777ca541d050fbe2da6db01efb5531
jcc <370234915@qq.com>
2021-08-03 12:47:39 +0800

    update

--- Modules/Backend/Resources/views/components/form/inputs/text.blade.php
+++ Modules/Backend/Resources/views/components/form/inputs/text.blade.php
@@ -20,5 +20,5 @@ x-init="init()"
 >
 
     <input x-ref="field" type="hidden" value="{{json_encode($field)}}">
-    <x-input x-bind:disabled="trigger_endable_or_disable()" wire:model="{{ $field['modelName']}}"  :id="$field['id']" :icon="$field['attributes']['field']['icon']??''"  :right-icon="$field['attributes']['field']['right-icon']??''"  :placeholder="$field['placeholder']" :hint="$field['comment']" :prefix="$field['attributes']['field']['prefix']??''" :suffix="$field['attributes']['field']['suffix']??''" :class="$field['cssClass'].' disabled:bg-gray-200'"/>
+    <x-input x-bind:disabled="trigger_endable_or_disable()" wire:model="{{ $field['modelName']}}"  :id="$field['id']" :icon="$field['attributes']['field']['icon']??''"  :right-icon="$field['attributes']['field']['right-icon']??''"  :placeholder="$field['placeholder']" :hint="$field['comment']?__($field['comment']):''" :prefix="$field['attributes']['field']['prefix']??''" :suffix="$field['attributes']['field']['suffix']??''" :class="$field['cssClass'].' disabled:bg-gray-200'"/>
 </div>
